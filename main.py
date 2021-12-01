37c067ac65c8403853ac44b501c001442c967f8c
Jeya Nishwanth <63254121+jnishwanth@users.noreply.github.com>
2021-12-01 14:19:48 +0530

    Revert "Input Changed"

--- main.py
+++ main.py
@@ -51,11 +51,10 @@ def get_link(episode):
     return resp['data'][-1]['1080']['kwik_adfly']
 
 def main():
-    anime = select_anime(input('Search for anime that you want to watch: '))
+    anime = select_anime(input('Search for anime: '))
     episode = select_episode(anime)
     link = get_link(episode)
     webbrowser.open(link)
-
     print(link)
     return link
 
